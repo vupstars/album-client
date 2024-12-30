832dfa5d20c6f059f51361b74fa5f801ad93cb96
MarryDream <2190758465@qq.com>
2024-12-31 04:36:24 +0800

    build: 初始化项目结构

---
+++ README.md
@@ -0,0 +1,33 @@
+# vupstars-album-client
+
+图床项目前端部分
+
+## 环境要求
+
+- `node` >= `20`
+
+## 项目启动
+
+安装依赖
+
+```shell
+pnpm install
+```
+
+启动项目
+
+```shell
+pnpm run dev
+```
+
+打包项目
+
+```shell
+pnpm run build
+```
+
+打包后预览
+
+```shell
+pnpm run serve
+```
---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { ConfigEnv, defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from "path";
+
+// https://vite.dev/config/
+export default ( { mode }: ConfigEnv ) => {
+	const env = loadEnv( mode, process.cwd() )
+	return defineConfig( {
+		base: env.VITE_PUBLIC_PATH,
+		plugins: [ vue() ],
+		resolve: {
+			alias: {
+				"@": path.join( __dirname, "./src" )
+			}
+		},
+		server: {
+			port: 60721,
+			open: true
+		},
+		optimizeDeps: {
+			exclude: [ "fsevents" ]
+		},
+		build: {
+			// https://cn.vitejs.dev/guide/build.html#browser-compatibility
+			target: "es2015",
+			sourcemap: false,
+			// 消除打包大小超过500kb警告
+			chunkSizeWarningLimit: 4000,
+			rollupOptions: {
+				input: {
+					index: path.join( __dirname, "index.html" )
+				},
+				// 静态资源分类打包
+				output: {
+					chunkFileNames: "static/js/[name]-[hash].js",
+					entryFileNames: "static/js/[name]-[hash].js",
+					assetFileNames: "static/[ext]/[name]-[hash].[ext]"
+				}
+			}
+		},
+	} )
+}
