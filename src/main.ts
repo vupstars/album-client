1d77e15d2ae4f6fa1fbe9c41805fe54543c38b8d
MarryDream <2190758465@qq.com>
2024-12-31 05:12:42 +0800

    feat: 初始化状态管理相关内容

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from "vue"
 import App from "./App.vue";
+import { createPinia } from "pinia";
 import router from "./router";
 import "./permission";
 
 const app = createApp( App );
+const pinia = createPinia();
 
 app.use( router )
+	.use( pinia )
 	.mount( "#app" );
